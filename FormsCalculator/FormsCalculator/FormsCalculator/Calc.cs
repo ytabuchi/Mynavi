210aad77109645d6689af3cac58726feb5f82cd9
ytabuchi @ Surface <ytabuchi.xlsoft@gmail.com>
2016-05-30 00:01:19 +0900

    Add FormsCalclator Project

---
+++ FormsCalculator/FormsCalculator/FormsCalculator/Calc.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FormsCalculator
+{
+    public static class Calc
+    {
+        public static double GetTax(double d)
+        {
+            return Math.Floor(d * 1.08);
+        }
+    }
+}
---
+++ FormsCalculator/FormsCalculator/FormsCalculator/CalcPage.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+
+namespace FormsCalculator
+{
+    public partial class CalcPage : ContentPage
+    {
+        public CalcPage()
+        {
+            InitializeComponent();
+
+            calcButton.Clicked += (sender, e) =>
+            {
+                double d;
+                if (double.TryParse(priceText.Text, out d))
+                    totalPriceText.Text = $"{Calc.GetTax(d)}円";
+            };
+        }
+    }
+}
